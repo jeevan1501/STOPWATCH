609f5a31a74bdc4338187c2235560d2ea6df72aa
Jeevan Kumar <76511509+jeevan1501@users.noreply.github.com>
2023-02-20 19:20:06 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import StopWatch from './components/StopWatch/StopWatch';
+
+function App() {
+return (
+	<div className="App">
+  <h1>React StopWatch</h1>
+	<StopWatch />
+	</div>
+);
+}
+
+export default App;
